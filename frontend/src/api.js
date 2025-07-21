a3c69f6ce58399d1b074361d9d1b33a96eb0d47b
Shashwat <shashwat.kul@gmail.com>
2025-07-21 14:11:17 +0530

    URL changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 // Create an instance of axios
+// Change the baseURL to use an environment variable
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // Your backend URL
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
 });
 
 // Add a request interceptor to include the token in all requests
