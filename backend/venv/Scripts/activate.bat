bb95606ba8c996f291642391154cb2a6b52491fe
Shashwat <shashwat.kul@gmail.com>
2025-08-11 14:43:53 +0530

    feat: Migrate holidays to a persistent database

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ABCD\OneDrive\Desktop\my-attendance-app\backend\venv
+set VIRTUAL_ENV=C:\Users\ABCD\OneDrive\Desktop\Attendance Portal\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
